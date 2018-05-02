115e5d3a1e54470c45b333755144c888ac7f9122
Fin Christensen <christensen.fin@gmail.com>
2018-05-02 16:10:17 +0200

    Minor changes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate lopdf;
 
-use lopdf::{Document};
+use lopdf::{Document, Object};
 
 fn main() {
     let mut doc1 = Document::load("1.pdf").unwrap();
@@ -8,9 +8,14 @@ fn main() {
 
     doc1.version = "1.4".to_string();
 
-    for (_, object_ids) in doc2.get_pages() {
-        doc1.add_object(object_ids);
-    }
+    let (_, last_page_id) = doc1.get_pages().into_iter().last().unwrap();
+    let (_, first_page_id) = doc2.get_pages().into_iter().next().unwrap();
+    let page_id = doc1.add_object(first_page_id);
+    doc1.get_dictionary(page_id)
+        .unwrap()
+        .get_mut("Parent")
+        .unwrap() = Object::From(&mut last_page_id);
+    doc1.renumber_objects();
 
     doc1.save("modified.pdf").unwrap();
 }
