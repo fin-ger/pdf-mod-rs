34647ff000a8f39db8b74010a99fb731ed534581
Fin Christensen <christensen.fin@gmail.com>
2018-05-02 16:58:25 +0200

    Print pages of each document

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,33 @@ extern crate lopdf;
 
 use lopdf::{Document, Object};
 
+fn get_pages<'a>(doc: &'a Document) -> Vec<&'a Object> {
+    doc
+        .get_object(
+            doc
+                .catalog()
+                .unwrap()
+                .get("Pages")
+                .unwrap()
+                .as_reference()
+                .unwrap()
+        )
+        .unwrap()
+        .as_dict()
+        .unwrap()
+        .get("Kids")
+        .unwrap()
+        .as_array()
+        .unwrap()
+        .into_iter()
+        .map(|kid| doc.get_object(kid.as_reference().unwrap()).unwrap())
+        .collect()
+}
+
 fn main() {
-    let mut doc1 = Document::load("1.pdf").unwrap();
+    let doc1 = Document::load("1.pdf").unwrap();
     let doc2 = Document::load("2.pdf").unwrap();
 
-    doc1.version = "1.4".to_string();
-
-    let (_, last_page_id) = doc1.get_pages().into_iter().last().unwrap();
-    let (_, first_page_id) = doc2.get_pages().into_iter().next().unwrap();
-    let page_id = doc1.add_object(first_page_id);
-    doc1.get_dictionary(page_id)
-        .unwrap()
-        .get_mut("Parent")
-        .unwrap() = Object::From(&mut last_page_id);
-    doc1.renumber_objects();
-
-    doc1.save("modified.pdf").unwrap();
+    println!("{:#?}", get_pages(&doc1));
+    println!("{:#?}", get_pages(&doc2));
 }
